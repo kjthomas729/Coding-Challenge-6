2e438d9a51092d1e1fb5a9c89025c2fece23cd8d
Kamali Thomas <kjthomas3@usf.edu>
2024-06-05 02:06:23 +0000

    Implement draw method for Ball class

--- script.js
+++ script.js
@@ -7,7 +7,18 @@ class Ball {
         this.dy = dy;
         this.color = color;
     }
+
+    draw(ctx) {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.closePath();
+    }
 }
 
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 const ball = new Ball(200, 160, 20, 2, 2, 'red');
-console.log(ball);
+
+ball.draw(ctx);
